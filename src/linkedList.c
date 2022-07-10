1ee749a76fd9a5d9b4a125c60eb6aa651000a4ff
Guerreiro51 <alessandrog54@usp.br>
2022-07-10 14:46:41 -0300

    Extra pointer security for linked list

--- src/linkedList.c
+++ src/linkedList.c
@@ -1,7 +1,7 @@
 #include "../header/linkedList.h"
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 Node* linkedListInit() {
     Node* head = (Node*)malloc(sizeof(Node));
@@ -12,65 +12,66 @@ Node* linkedListInit() {
 }
 
 void linkedListDestroy(Node** head) {
-
-    if ( linkedIsEmpty(*head) )
+    if (!head)
         return;
-        
-    linkedListDestroy( &((*head)->next) );
+
+    if (!(*head))
+        return;
+
+    linkedListDestroy(&((*head)->next));
     free(*head);
 }
 
 void linkedListPushBack(Node** head) {
-    if(!head)
+    if (!head)
         return;
 
-    if ( linkedIsEmpty(*head) ) { // empty list, become head
+    if (!(*head)) {  // empty list, become head
         *head = linkedListInit();
         return;
     }
-    if(!(*head)->next) { // last node, pushback
+    if (!(*head)->next) {  // last node, pushback
         (*head)->next = linkedListInit();
     } else {
-        linkedListPushBack( &(*head)->next );
+        linkedListPushBack(&(*head)->next);
     }
 }
 
-int linkedListPeak(Node* head){
-    if(linkedIsEmpty(head))
+int linkedListPeak(Node* head) {
+    if (!head)
         return -1;  // not a synchronization token
 
-    if(!head->next)
-        return head->depth;  
-    
-    return linkedListPeak( head->next );
+    if (!head->next)
+        return head->depth;
 
+    return linkedListPeak(head->next);
 }
 
-
 void linkedListPop(Node** head) {
-    if (linkedIsEmpty(*head))
+    if (!(*head))
         return;
-    if( !(*head)->next ) {
+
+    if (!(*head)->next) {
         free(*head);
         (*head) = NULL;
         return;
     }
-    if(!(*head)->next->next) {  // next is last element
+
+    if (!(*head)->next->next) {  // next is last element
         free((*head)->next);
         (*head)->next = NULL;
     } else {
         linkedListPop(&((*head)->next));
     }
-    
-}
-
-bool linkedIsEmpty(Node* head) {
-    return (head == NULL);
 }
 
 void linkedListAdd(Node** head, int toAdd) {
-    if (linkedIsEmpty(*head))
+    if (!head)
         return;
+
+    if (!(*head))
+        return;
+
     (*head)->depth += toAdd;
     linkedListAdd(&((*head)->next), toAdd);
 }
